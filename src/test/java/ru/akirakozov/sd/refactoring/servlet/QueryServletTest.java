b8b7f8e629230b4250a7b4fcd98eb3ac79b5f60d
Ilona Bozhe <warmte00@gmail.com>
2022-11-06 22:44:38 +0300

    add tests for QueryServlet

---
+++ src/test/java/ru/akirakozov/sd/refactoring/servlet/QueryServletTest.java
@@ -0,0 +1,17 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author akirakozov
+ */
+public class QueryServletTest extends ServletTest {
+
+}
